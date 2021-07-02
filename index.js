cf84a1fe9b79dac803c67773d225dd745d5a9568
ApoloGH <apolo-gh@users.noreply.github.com>
2021-07-02 18:00:27 -0400

    refactor: fix linting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import nodeFetch from 'node-fetch';
-import nodeAbortController from 'abort-controller'
+import nodeAbortController from 'abort-controller';
 
-const abortController = (typeof AbortController === 'undefined' ? nodeAbortController : AbortController)
+const abortController = typeof AbortController === 'undefined' ? nodeAbortController : AbortController;
 
 export default typeof fetch === 'undefined' ? nodeFetch : fetch;
-export { abortController as AbortController }
+export { abortController as AbortController };
