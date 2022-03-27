9f5fceeae0372f77649d2eada979c98bea8f0cf0
ApoloGH <apolo-gh@users.noreply.github.com>
2022-03-27 13:18:14 -0400

    chore: upgrade rollup

--- dist/index.cjs
+++ dist/index.cjs
@@ -10,8 +10,8 @@ function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'defau
 var nodeFetch__default = /*#__PURE__*/_interopDefaultLegacy(nodeFetch);
 var nodeAbortController__default = /*#__PURE__*/_interopDefaultLegacy(nodeAbortController);
 
-const abortController = typeof AbortController === 'undefined' ? nodeAbortController__default['default'] : AbortController;
-const exportFetch = typeof fetch === 'undefined' ? nodeFetch__default['default'] : fetch;
+const abortController = typeof AbortController === 'undefined' ? nodeAbortController__default["default"] : AbortController;
+const exportFetch = typeof fetch === 'undefined' ? nodeFetch__default["default"] : fetch;
 
 exports.AbortController = abortController;
 exports.fetch = exportFetch;
